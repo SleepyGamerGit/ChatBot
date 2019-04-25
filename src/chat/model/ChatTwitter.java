adf71ab33ab8b8037efe5c08aff7f5fa0460d6be
Sleepy <hillsteadm@gmail.com>
2019-04-25 09:48:59 -0600

    adding twitter framework

--- src/chat/model/ChatTwitter.java
+++ src/chat/model/ChatTwitter.java
@@ -1,13 +1,95 @@
 package chat.model;
 
 import chat.controller.ChatController;
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.text.DecimalFormat;
+
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.GeoLocation;
+import twitter4j.Paging;
+import twitter4j.Query;
+import twitter4j.QueryResult;
+import twitter4j.ResponseList;
+import twitter4j.Status;
+
+import chat.controller.IOController;
 
 public class ChatTwitter
 {
     private ChatController app;
+    private Twitter chatTwitter;
+    private List<Status> searchedTweets;
+    private List<String> tweetedWords;
+    private long totalWordCount;
+    private HashMap<String, Integer> wordsAndCount;
     
     public ChatTwitter(ChatController app)
     {
     	this.app = app;
     }
+    
+    public void sendTweet(String textToTweet)
+    {
+    	try
+    	{
+    		chatTwitter.updateStatus(textToTweet + " @ChatbotCTEC");
+    	}
+    	catch(TwitterException tweetError)
+    	{
+    		app.handleErrors(tweetError);
+    	}
+    	catch(Exception otherError)
+    	{
+    		app.handleErrors(otherError);
+    	}
+    }
+    
+    private void collectTweets(String username)
+    {
+    	Paging statusPage = new Paging(1,100);
+    	int page = 1;
+    	long lastID = Long.MAX_VALUE;
+    	
+    	while(page <= 10)
+    	{
+    		statusPage.setPage(page);
+    		try
+    		{
+    			ResponseList<Status> listedTweets = chatTwitter.getUserTimeline(username, statusPage);
+    			for(Status current : listedTweets)
+    			{
+    				if(current.getId() < lastID)
+    				{
+    					searchedTweets.add(current);
+    					lastID = current.getId();
+    				}
+    			}
+    		}
+    		catch(TwitterException searchTweetError)
+    		{
+    			app.handleErrors(searchTweetError);
+    		}
+    		page++;
+    	}
+    }
+    
+    private void turnStatusesToWords()
+    {
+    	for(Status currentStatus : searchedTweets)
+    	{
+    		String punctuation = ".,':;\"() {}^[]<>-";
+    		
+    		String scrubbedString = "";
+    		for(int i = 0; i < currentString.length(); i++)
+    		{
+    			if(punctuation.indexOf(currentString.charAt(i)) == -1)
+    		}
+    	}
+    }
+    
 }
